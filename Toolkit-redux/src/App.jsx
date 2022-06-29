9efda24bb60983eadaa11743e7480e3dd3d37723
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-28 23:19:53 -0400

    Add: Hello World

--- Toolkit-redux/src/App.jsx
+++ Toolkit-redux/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
+          <h2>Hello World</h2>
         </p>
         
       </header>
