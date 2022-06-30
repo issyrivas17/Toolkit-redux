12bc49962578c5fe8d4fa7681b8635adeeca5e18
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-30 10:40:42 -0400

    instalacion redux

--- Toolkit-redux/src/App.jsx
+++ Toolkit-redux/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
-          <h2>Hello</h2>
+          <h2>Hello my name es chiki chiki chiki chiki </h2>
         </p>
       </header>
     </div>
