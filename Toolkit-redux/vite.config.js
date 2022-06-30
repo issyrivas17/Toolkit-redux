aa408d5923bfd538fb881fd683093a62aab794e4
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-30 10:32:23 -0400

    me falta instalar redux

--- Toolkit-redux/vite.config.js
+++ Toolkit-redux/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(),  checker({
+    eslint: {
+      lintCommand: 'eslint "./src/**/*.{js,jsx}"', // for example, lint .ts & .tsx
+    },
+  })]
 })
