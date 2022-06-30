dbf9e6f7cbe849e152e03d4fe60f05333d4e8450
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-06-30 10:46:09 -0400

    se pasa un store al provider, se toma la exportacion del index.js y no es necesario que se haga una importacion en el main

--- Toolkit-redux/src/main.jsx
+++ Toolkit-redux/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
   <React.StrictMode>
-  <Provider>
+  <Provider store= {store}> 
     <App />
   </Provider>
   </React.StrictMode>
