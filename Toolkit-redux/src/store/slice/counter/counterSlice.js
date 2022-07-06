421d8db39c2dce27bad35b7942b843c042fccc53
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-06 15:59:25 -0400

    se borro el decrement y el increment porque no se van a utilizar

--- Toolkit-redux/src/store/slice/counter/counterSlice.js
+++ Toolkit-redux/src/store/slice/counter/counterSlice.js
@@ -15,14 +15,9 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      state.counter +=1; 
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+   
   },
 })
 
