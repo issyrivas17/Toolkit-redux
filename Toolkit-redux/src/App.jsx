fe867b48ae86a61d41e547511068c92b626d347a
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-06 19:37:46 -0400

    creando un snippet y gists de slice

--- Toolkit-redux/src/App.jsx
+++ Toolkit-redux/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
             Increment 
           </button> 
 
-          <button type="button" onClick={ () => dispacth (incrementBy(3) ) }>
+          <button type="button" onClick={ () => dispacth (incrementBy(2) ) }>
             IncrementBy 2 
           </button> 
 
