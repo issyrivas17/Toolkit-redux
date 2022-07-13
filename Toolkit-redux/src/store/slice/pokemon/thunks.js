970548dcc46d21e83b9e0bd4cf8edfe48952ad6a
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-13 14:55:33 -0400

    problemas con el await me indica que es un identificador reservado

--- Toolkit-redux/src/store/slice/pokemon/thunks.js
+++ Toolkit-redux/src/store/slice/pokemon/thunks.js
@@ -6,8 +6,9 @@ export const getPokemons = (page =0 ) => {
         dispatch (starLoandingPokemons () ); 
         //TODO: Realizar Peticion http
 
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=12&offset=${page*10}`); 
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10 }`); 
         const data = await resp.json (); 
+        console.log(data); 
 
         dispatch (setPokemons ({pokemons:data.results,page : page +1 })) 
     }
