5e55bb35e67b958c6653e0aebacfd9cdd8df25cb
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-16 21:32:16 -0400

    se le pasa pokemonApi al thunks y se hace console.log a ver si toma funciona

--- Toolkit-redux/src/store/slice/pokemon/thunks.js
+++ Toolkit-redux/src/store/slice/pokemon/thunks.js
@@ -1,4 +1,5 @@
 /* eslint-disable */ 
+import { pokemonApi } from "../../../API/PokemonApi";
 import { setPokemons, starLoandingPokemons } from "./pokemonSlice"
 
 export const getPokemons = (page =0 ) => {
@@ -6,8 +7,11 @@ export const getPokemons = (page =0 ) => {
         dispatch (starLoandingPokemons () ); 
         //TODO: Realizar Peticion http
 
-     const resp = await fetch (`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10 }`)
-        const data = await resp.json (); 
+     /* const resp = await fetch (`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10 }`)
+        const data = await resp.json ();  */
+
+       const resp= await pokemonApi.get(`pokemon?limit=10&offset=${page * 10 }`)
+       console.log(resp); 
         
 
 
