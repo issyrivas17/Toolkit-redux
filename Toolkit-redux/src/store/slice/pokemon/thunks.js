658ee91d89a6b37741b52c66a83bebe361d08b79
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-16 21:28:45 -0400

    se instalo axios y se esta pasando por PokemonApi la peticion https

--- Toolkit-redux/src/store/slice/pokemon/thunks.js
+++ Toolkit-redux/src/store/slice/pokemon/thunks.js
@@ -8,7 +8,8 @@ export const getPokemons = (page =0 ) => {
 
      const resp = await fetch (`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10 }`)
         const data = await resp.json (); 
-        console.log(data); 
+        
+
 
         dispatch (setPokemons ({pokemons:data.results,page : page +1 })) 
     }
