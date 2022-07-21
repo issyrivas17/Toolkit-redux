363b0d0a74a5291769c6aa1f78bed6b892d2a746
Ismenia Rivas <ismeniarivas17@gmail.com>
2022-07-20 23:04:59 -0400

    comentamos PokemonApp y pasamos TodoApp en la parte mas alta de nuestraAp

--- Toolkit-redux/src/main.jsx
+++ Toolkit-redux/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App'
 import './index.css'
 import { PokemonApp } from './PokemonApp'
 import { store } from './store'
+import { TodoApp } from './TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
@@ -12,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
     <Provider store= {store}> 
   {/*     <App /> */} 
-  <PokemonApp/> 
+  {/* <PokemonApp/>  */} 
+  <TodoApp/> 
     </Provider>
     </React.StrictMode>
   )
